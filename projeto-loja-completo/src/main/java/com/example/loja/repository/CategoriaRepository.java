01ce03cb3b8c806a9c20b3781c7c6f814051bf59
viniciusalexandre2 <vinicius141103a@gmail.com>
2025-04-25 00:19:13 -0300

    Delete projeto-loja-completo directory

--- projeto-loja-completo/src/main/java/com/example/loja/repository/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-
-package com.example.loja.repository;
-
-import com.example.loja.model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {}
--- projeto-loja-completo/src/main/java/com/example/loja/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.loja.repository;
-
-import com.example.loja.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
