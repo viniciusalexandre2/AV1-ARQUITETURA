0371a6fecfed6b9148812fd923a7a9c009b5951c
viniciusalexandre2 <vinicius141103a@gmail.com>
2025-04-25 00:24:44 -0300

    Add files via upload

---
+++ projeto-loja-av1/src/main/java/com/example/loja/repository/CategoriaRepository.java
@@ -0,0 +1,7 @@
+
+package com.example.loja.repository;
+
+import com.example.loja.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {}
